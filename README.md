c40e67dc3767f0c1ba367a2c4cbc99b3c4dbf79e
eyalzek <eyal.ez@gmail.com>
2015-12-19 13:17:01 +0100

    should work, but untested

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Script to automate the creation of a transmission server, exposing a webui and ftp access.
+## Script to automate the creation of a transmission server, exposing a webui and ftp access.
+### to run (considering you are logged in as root):
+`wget https://raw.githubusercontent.com/eyalzek/trserver-startup/master/run.sh -v -O run.sh && ./run.sh; rm run.sh`
--- run.sh
+++ run.sh
@@ -12,16 +12,18 @@ USERDOWNLOADDIR="$USERHOME/Downloads"
 REPODIR="$USERHOME/repos"
 adduser $USERNAME
 
-# switch to user and create Downloads folder
-su $USERNAME
-cd
-mkdir Downloads
-exit
-
 # update and download packages
 echo 'updating and installing required packages'
 apt-get update -y
-apt-get install -y git nano htop transmission-cli transmission-common transmission-daemon vsftpd
+apt-get install -y curl git nano htop transmission-cli transmission-common transmission-daemon vsftpd
+
+# switch to user and create folders
+su $USERNAME
+cd $USERHOME
+mkdir Downloads repos bin
+cd $REPODIR && git clone git@github.com:eyalzek/trserver-startup.git
+exit
+cd "$REPODIR/trserver-startup"
 
 # stop services
 echo 'stopping services in order to configure'
@@ -43,7 +45,6 @@ cp settings.json /etc/transmission-daemon/
 # update and copy the auto-clear finished torrents script
 perl -pi -e 's/REPLACEWITHUSER/$USERNAME' clear-finished.sh
 perl -pi -e 's/REPLACEWITHPASS/$TRPASS' clear-finished.sh
-mkdir "$USERHOME/bin"
 cp clear-finished.sh "$USERHOME/bin/"
 # copy vsftpd config
 cp vsftpd.conf /etc/
@@ -57,9 +58,10 @@ start vsftpd
 cd $REPODIR
 git clone git@github.com:eyalzek/pirate-get.git && cd pirate-get
 ./install
-cd
 
 # test connection?
 
-# get external ip and print relevant address?
-# echo 'transmission webui availble on: '
+# get external ip and print relevant address
+EXTIP=$(curl ident.me)
+echo 'transmission webui availble on: '
+echo '$EXTIP:9091/transmission/web'
